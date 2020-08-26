9ad610f69ac03ed251ab4e3fb238858f66d30d97
kahenya-anita <anitakahenya1@gmail.com>
2020-08-26 16:54:01 +0300

    Worked on routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotlyComponent } from './components/quotly/quotly.component';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
+import { QuoteDetailsComponent } from './components/quote-details/quote-details.component';
+import { DateSubmittedPipe } from './pipes/date-submitted.pipe';
+import { HighlightVoteDirective } from './directives/highlight-vote.directive';
+import { HighlightQuoteDirective } from './directives/highlight-quote.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotlyComponent,
+    QuoteFormComponent,
+    QuoteDetailsComponent,
+    DateSubmittedPipe,
+    HighlightVoteDirective,
+    HighlightQuoteDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
